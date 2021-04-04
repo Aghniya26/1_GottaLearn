25deb7aa5e1e9531bb8db48049929d0de43d6362
aghniya rahma yudha <you@example.com>
2021-04-05 02:41:32 +0700

    first commit

---
+++ app/src/main/java/com/example/gottalearn/ExampleAdapter.kt
@@ -0,0 +1,57 @@
+package com.example.gottalearn
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ExampleAdapter(private val exampleList: List<ExampleItem>,
+                     private val listener: OnItemClickListerner
+) :
+    RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExampleViewHolder {
+        val itemView=LayoutInflater.from(parent.context).inflate(R.layout.example_item,
+            parent, false)
+
+        return ExampleViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ExampleViewHolder, position: Int) {
+        val currentItem=exampleList[position]
+
+        holder.imageView.setImageResource(currentItem.imageResource)
+        holder.textView1.text=currentItem.text1
+        holder.textView2.text=currentItem.text2
+
+
+
+    }
+
+    override fun getItemCount()=exampleList.size
+
+    inner class ExampleViewHolder (itemView: View): RecyclerView.ViewHolder (itemView),
+    View.OnClickListener{
+        val imageView: ImageView=itemView.findViewById(R.id.bookmark)
+        val textView1: TextView=itemView.findViewById(R.id.text_view1)
+        val textView2: TextView=itemView.findViewById(R.id.text_view2)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val position=adapterPosition
+            if(position!=RecyclerView.NO_POSITION){
+                listener.onItemClick(position)
+            }
+
+        }
+    }
+
+    interface OnItemClickListerner{
+        fun onItemClick(position: Int)
+    }
+}
---
+++ app/src/main/java/com/example/gottalearn/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.gottalearn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity(), ExampleAdapter.OnItemClickListerner {
+    private val exampleList= generateDummyList(500)
+    private val adapter=ExampleAdapter(exampleList, this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+       val recyclerView : RecyclerView = findViewById(R.id.recycle_view)
+        recyclerView.adapter=adapter
+        recyclerView.layoutManager=LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+
+    }
+    fun insertItem(view: View){
+        val index= Random.nextInt(8)
+        val newItem=ExampleItem(
+            R.drawable.ic_bookmark_yellow,
+            "New item at Position $index",
+            "line2"
+
+        )
+        exampleList.add(index, newItem)
+        adapter.notifyItemInserted(index)
+    }
+    fun removeItem(view: View){
+        val index= Random.nextInt(8)
+        exampleList.removeAt(index)
+        adapter.notifyItemRemoved(index)
+    }
+
+    override fun onItemClick(position: Int) {
+        Toast.makeText(this, "Item $position Clicked", Toast.LENGTH_SHORT).show()
+        val clickedItem=exampleList[position]
+        clickedItem.text1="Clicked"
+        adapter.notifyItemChanged(position)
+    }
+
+    private fun generateDummyList (size: Int): ArrayList<ExampleItem>{
+        val list=ArrayList<ExampleItem>()
+
+        for (i in 0 until size){
+            val drawable= when (i % 4){
+                0 -> R.drawable.ic_bookmark_green
+                1 -> R.drawable.ic_bookmark_orange
+                2 -> R.drawable.ic_bookmark_yellow
+                else -> R.drawable.ic_bookmark_red
+            }
+            val item = ExampleItem(drawable, text1 = "Recyle View $i", text2 = "$i d 2h")
+            list+=item
+
+        }
+        return list
+    }
+}
